d673145d8ae30781dfa5c704e7a8fe1f8d8dd0b5
jafar <mirzaraximovjafar2001@gmail.com>
2022-07-07 23:30:21 +0500

    salom yengi file yaratdim

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const App = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 import React from "react";
-imp
+import ReactDOM from 'react-dom'
+import './index.css'
+
+import App from './App'
+
+ReactDOM.render(
+  <App />, document.getElementById('root')
+) 

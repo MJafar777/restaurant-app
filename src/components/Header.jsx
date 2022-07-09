4a5f49eb88d26483ef766707082867e7d8bf3843
jafar <mirzaraximovjafar2001@gmail.com>
2022-07-09 14:51:32 +0500

    projectni 15:00 da tugatildi

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,34 @@
 import React from "react";
-
+import Logo from "./img/logo.png";
 const Header = () => {
   return (
-    <div className="fixed z-50 w-screen bg-slate-300 p-6 px-16">
+    <header className="fixed z-50 w-screen  p-6 px-16">
       {/* desctop & tablet*/}
-      <div className="hidden md:flex w-full h-full bg-red-600 p-4"></div>
+      <div className="hidden md:flex w-full h-full ">
+        <div className="flex items-center gap-2">
+          <img src={Logo} className="w-8 object-cover" alt="logo" />
+          <p className="text-headingColor text-xl font-bold"> City</p>
+        </div>
+
+        <ul className="flex items-center gap-8 ">
+          <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+            Home
+          </li>
+          <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+            Menu
+          </li>
+          <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+            About Us
+          </li>
+          <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+            Service
+          </li>
+          <li></li>
+        </ul>
+      </div>
       {/* mobile*/}
-      <div className="flex md:hidden w-full h-full bg-blue-500 p-4"></div>
-    </div>
+      <div className="flex md:hidden w-full h-full "></div>
+    </header>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App";
-
-ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById("root")
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>
 );

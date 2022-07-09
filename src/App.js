92553622dba276048aa7fccc7196cef92a2479bf
jafar <mirzaraximovjafar2001@gmail.com>
2022-07-09 14:15:12 +0500

    add Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react";
+import { Header } from "./components";
 
 const App = () => {
-  return (
-    <div className='w-screen h-screen flex items-center justify-center text-blue-600'>App</div>
-  )
-}
+  return <div className="w-screen h-auto flex flex-col">
+    <Header/>
+  </div>;
+};
 
-export default App
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="fixed z-50 w-screen bg-slate-300 p-6 px-16">Header</div>
+  );
+};
+
+export default Header;

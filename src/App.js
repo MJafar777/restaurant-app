7762b86d133ad9c08613af9fbb8b2ad6b87327ac
jafar <mirzaraximovjafar2001@gmail.com>
2022-07-09 14:03:52 +0500

    create new files and libraries

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='w-screen h-screen flex items-center justify-center text-blue-600'>App</div>
   )
 }
 

6db9d3a9190d653ecdb95a0971ca094043220f3b
jafar <mirzaraximovjafar2001@gmail.com>
2022-07-09 14:25:38 +0500

    create classes Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,12 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="fixed z-50 w-screen bg-slate-300 p-6 px-16">Header</div>
+    <div className="fixed z-50 w-screen bg-slate-300 p-6 px-16">
+      {/* desctop & tablet*/}
+      <div className="hidden md:flex w-full h-full bg-red-600 p-4"></div>
+      {/* mobile*/}
+      <div className="flex md:hidden w-full h-full bg-blue-500 p-4"></div>
+    </div>
   );
 };
 

392243b1e490d441c38ccca4c6521e5e5610f158
jafar <mirzaraximovjafar2001@gmail.com>
2022-07-10 18:54:10 +0500

    classes

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -6,7 +6,7 @@ const MainContainer = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
       <div className="py-2  flex-1 flex flex-col items-start md:items-center justify-center">
-        <div className="flex items-center gap-2 justify-center bg-orange-100 p-2 ">
+        <div className="flex items-center gap-2 justify-center bg-orange-100 p-2 rounded-full">
           <p>Bike Delivery</p>
           <div className="w-6 h-6 bg-white rounded-full overflow-hidden">
             <img

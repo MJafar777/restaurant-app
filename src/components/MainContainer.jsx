2efa73aee14358d825be54ccabb0cd98a9cc8dcb
jafar <mirzaraximovjafar2001@gmail.com>
2022-07-10 18:38:45 +0500

    mainContainer

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 
+import Delivery from "../img/delivery.png";
+
 const MainContainer = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
-      <div className="p-4 bg-blue-400 flex-1"></div>
-      <div className="p-4 bg-blue-400 flex-1"></div>
+      <div className="py-2  flex-1 flex flex-col items-start md:items-center justify-center">
+        <div className="flex items-center gap-2 justify-center bg-orange-100 p-2 ">
+          <p>Bike Delivery</p>
+          <div className="w-6 h-6 bg-white rounded-full overflow-hidden">
+            <img
+              src={Delivery}
+              alt="Delivery"
+              className="w-full h-full object-contain"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="py-2 bg-blue-400 flex-1"></div>
     </div>
   );
 };

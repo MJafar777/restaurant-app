2517f9aef50ec9b57455dd3cb450c661c5fe387e
jafar <mirzaraximovjafar2001@gmail.com>
2022-07-10 20:12:20 +0500

    main container

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -5,10 +5,12 @@ import Delivery from "../img/delivery.png";
 const MainContainer = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
-      <div className="py-2  flex-1 flex flex-col items-start md:items-center justify-center">
-        <div className="flex items-center gap-2 justify-center bg-orange-100 p-2 rounded-full">
-          <p>Bike Delivery</p>
-          <div className="w-6 h-6 bg-white rounded-full overflow-hidden">
+      <div className="py-2  flex-1 flex flex-col items-start md:items-center justify-center gap-6">
+        <div className="flex items-center gap-2 justify-center bg-orange-100 px-2 py-1 rounded-full">
+          <p className="text-base text-orange-500 font-semibold">
+            Bike Delivery
+          </p>
+          <div className="w-6 h-6 bg-white rounded-full overflow-hidden drop-shadow-xl">
             <img
               src={Delivery}
               alt="Delivery"
@@ -16,6 +18,16 @@ const MainContainer = () => {
             />
           </div>
         </div>
+        <p className="text-[2.5rem] font-bold tracking-wide text-headingColor">
+          The Fastest Delivery in{" "}
+          <span className="text-orange-600 text-[3rem]">Your City</span>
+        </p>
+        <p className="text-base text-textColor">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam illum
+          quisquam perferendis cupiditate, reiciendis sequi amet quo voluptatum
+          ut, placeat harum nisi provident unde, nihil id corporis a suscipit
+          perspiciatis.
+        </p>
       </div>
       <div className="py-2 bg-blue-400 flex-1"></div>
     </div>
